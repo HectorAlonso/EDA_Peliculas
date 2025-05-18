8570e0be8b1e200c8835684adde2eba8b645dcde
Hector Alonso <aloarenas98@gmail.com>
2025-05-17 18:11:16 -0600

    Exploración general

--- DataMovie.py
+++ DataMovie.py
@@ -55,3 +55,44 @@ data["Cast"] = data["Cast"].apply(ast.literal_eval)
 print(f"\nEl tipo de datos de Genres es: {type(data['Genres'][0])}")
 print(f"El tipo de datos de Countries es: {type(data['Countries'][0])}")
 print(f"El tipo de datos de Cast es: {type(data['Cast'][0])}")
+
+# ! 2- Exploración general -------------------------------------------------
+
+# ? ¿Cuál es la calificación promedio general de las películas? ---------
+
+promedio_general = data["Average_rating"].mean()
+print(f"La calificacion promedio general de las peliculas es: {promedio_general.round(2)}")
+
+# ? ¿Qué porcentaje tiene más de 100,000 ratings? -----------------------
+
+row_con_mas_rating = data[data["Total_ratings"] >= 100000]
+row_con_mas_rating = row_con_mas_rating.shape[0]
+
+
+totalregistros = data.shape[0]
+porcentaje = (row_con_mas_rating / totalregistros) * 100
+# print(f"El tipo de dato de proncentaje es: {type(porcentaje)}")
+porcentaje = round(porcentaje,2)
+
+print(f"Total de Registros: {totalregistros}")
+print(f"El numero de registros con mas de 100,000 de rating es: {row_con_mas_rating}")
+print(f"El porcentaje con mas de 100,000 de rating es: {porcentaje}%")
+
+# ? ¿Cuáles son los directores más frecuentes? ---------------------------
+
+DirectoresFrecuntes = data["Director"].value_counts()
+
+print("Los directores mas frecuentes son: \n")
+print(DirectoresFrecuntes)
+
+# ? ¿Qué películas tienen calificación promedio más alta y más baja? ----
+
+calif_Mas_Alta = data["Average_rating"].max()
+calif_Mas_Baja = data["Average_rating"].min()
+pelicula_Calif_Mas_Alta = data[data["Average_rating"] == calif_Mas_Alta]
+pelicula_Calif_Mas_Baja = data[data["Average_rating"] == calif_Mas_Baja]
+
+print(f"La pelicula con la calificacion mas Alta es: ")
+print(pelicula_Calif_Mas_Alta[["Film_title","Average_rating"]])
+print(f"\nLa pelicula con la calificacion mas Baja es: ")
+print(pelicula_Calif_Mas_Baja[["Film_title","Average_rating"]])
